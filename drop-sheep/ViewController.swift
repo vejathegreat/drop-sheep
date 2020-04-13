de1f2751c8542e0a2ec3034bef68622a8a468689
vejathegreat <velly.mathebula28@gmail.com>
2020-04-13 22:00:18 +0200

    Initial Commit

---
+++ drop-sheep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drop-sheep
+//
+//  Created by Velaphi Mathebula on 2020/04/13.
+//  Copyright © 2020 Velaphi Mathebula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ drop-sheepTests/drop_sheepTests.swift
@@ -0,0 +1,34 @@
+//
+//  drop_sheepTests.swift
+//  drop-sheepTests
+//
+//  Created by Velaphi Mathebula on 2020/04/13.
+//  Copyright © 2020 Velaphi Mathebula. All rights reserved.
+//
+
+import XCTest
+@testable import drop_sheep
+
+class drop_sheepTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
