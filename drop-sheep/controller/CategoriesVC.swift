5fb82c2b648cb95772e0d450f76e342e0172c26a
Velaphi Mathebula <velly.mathebula28@gmail.com>
2020-04-14 08:14:17 +0200

    added table view

---
+++ drop-sheep/controller/CategoriesVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  drop-sheep
+//
+//  Created by Velaphi Mathebula on 2020/04/13.
+//  Copyright © 2020 Velaphi Mathebula. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
